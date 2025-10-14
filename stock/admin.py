6dd5997bacee457cf2adbde99a261caff11bed7d
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-10-14 15:47:04 +0300

    added serializer file to each app

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Category, Products, PurchaseItems, Suppliers, Expense
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Products)
+admin.site.register(PurchaseItems)
+admin.site.register(Suppliers)
+admin.site.register(Expense)
