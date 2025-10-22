75d1993393dd9d68d917eec85771911ab5524cdf
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-10-22 20:12:46 +0300

    monthly and weekly report endpoint added

--- sales/views.py
+++ sales/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import timedelta
 from .models import SaleItems,SalesClerk, PaymentMethod, Products
 from accounts.permissions import IsAdmin,IsAdminOrOwner, IsSalesClerk, IsSalesClerkOrAdmin
 from rest_framework.authentication import TokenAuthentication
@@ -6,8 +8,10 @@ from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.generics import ListAPIView
 from rest_framework.exceptions import PermissionDenied
 from .serializers import SaleItemsSerializer
+from calendar import monthrange
 
 # Create your views here.
 class SalesItemViewset(viewsets.ModelViewSet):
@@ -41,4 +45,37 @@ class SalesItemViewset(viewsets.ModelViewSet):
         if self.request.user != 'admin':
             raise PermissionDenied("an admin is required!")
         instance.delete()
+
+@action(methods=['get'], detail=True)
+class WeeklyReportView(ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminOrOwner, IsAuthenticated]
+    queryset = SaleItems.objects.all().prefetch_related('sold_items', 'sales_by', 'sales_payment_method')
+    serializer_class = SaleItemsSerializer
+
+    def get_queryset(self):
+        today = timezone.now().date()
+        monday = today - timedelta(days=today.weekday())  
+        saturday = monday + timedelta(days=5)
+
+        data = SaleItems.objects.filter(date__range=[monday, saturday])
+        serializer_data = list(data.values())
+        return serializer_data
+    
+class MOnthlyReportView(ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminOrOwner, IsAuthenticated]
+    queryset = SaleItems.objects.all().prefetch_related('sold_items', 'sales_by', 'sales_payment_method')
+    serializer_class = SaleItemsSerializer
+
+    def get_queryset(self):
+        today = timezone.now().date()
+        start_month = today.replace(day=1)
+        end_month = today.replace(days=monthrange(today.year, today.month)[1])
+        
+        data = SaleItems.objects.filter(date__range=[start_month, end_month])
+        serializer_data = list(data.values())
+        return serializer_data
+    
+    
 
